82057dee6d60e3ee14b940e21c2b366440b8aa04
John Meena <johstamicrofinance@gmail.com>
2025-06-24 00:26:54 -0700

    Update Sequelize config to support DATABASE_URL and local env

--- config/config.js
+++ config/config.js
@@ -1,27 +1,22 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
   },
-
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: "postgres",
-    logging: false,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: false, // Required for Supabase pooled connections
       },
     },
-  },
+  }
 };
