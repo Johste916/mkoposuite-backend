5478bddd8b2a7ab8c3942956b937e6b74c081c10
John Meena <johstamicrofinance@gmail.com>
2025-07-04 12:25:31 +0300

    Fix: use DATABASE_URL in Sequelize config for GitHub Actions

--- config/config.js
+++ config/config.js
@@ -1,12 +1,28 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: "postgres"
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: "postgres"
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
