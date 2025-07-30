7331afa969ba7789d58b07806a70db6d3a6e0742
John Meena <johstamicrofinance@gmail.com>
2025-07-30 10:19:45 +0300

    Add missing borrower model

--- src/models/Borrower.js
+++ src/models/Borrower.js
@@ -1,27 +1,33 @@
-module.exports = (sequelize, DataTypes) => {
-  const Borrower = sequelize.define('Borrower', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    nationalId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true
-    }
-  });
+// src/models/borrower.js
 
-  return Borrower;
-};
+const { DataTypes } = require('sequelize');
+const sequelize = require('./db');
+
+const Borrower = sequelize.define('Borrower', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  nationalId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE
+});
+
+module.exports = Borrower;
