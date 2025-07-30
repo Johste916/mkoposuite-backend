708afcabb77b4f5ba3159e4439342069a182b305
John Meena <johstamicrofinance@gmail.com>
2025-07-30 10:44:32 +0300

    Backend working: DB connected, server running

--- src/models/Borrower.js
+++ src/models/Borrower.js
@@ -1,33 +1,34 @@
 // src/models/borrower.js
 
-const { DataTypes } = require('sequelize');
-const sequelize = require('./db');
+module.exports = (sequelize, DataTypes) => {
+  const Borrower = sequelize.define('Borrower', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nationalId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    branchId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
 
-const Borrower = sequelize.define('Borrower', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  nationalId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  address: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  createdAt: DataTypes.DATE,
-  updatedAt: DataTypes.DATE
-});
-
-module.exports = Borrower;
+  return Borrower;
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,17 @@
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 const db = {};
-
-// Initialize Sequelize
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
 // Import models
 db.User = require('./user')(sequelize, Sequelize.DataTypes);
@@ -27,35 +25,28 @@ db.LoanPayment = require('./loanpayment')(sequelize, Sequelize.DataTypes);
 // Model Associations
 // ======================
 
-// User ↔ Branch
 db.User.belongsTo(db.Branch, { foreignKey: 'branchId' });
 db.Branch.hasMany(db.User, { foreignKey: 'branchId' });
 
-// Borrower ↔ Branch
 db.Borrower.belongsTo(db.Branch, { foreignKey: 'branchId' });
 db.Branch.hasMany(db.Borrower, { foreignKey: 'branchId' });
 
-// Loan ↔ Borrower
 db.Loan.belongsTo(db.Borrower, { foreignKey: 'borrowerId' });
 db.Borrower.hasMany(db.Loan, { foreignKey: 'borrowerId' });
 
-// Loan ↔ Branch
 db.Loan.belongsTo(db.Branch, { foreignKey: 'branchId' });
 db.Branch.hasMany(db.Loan, { foreignKey: 'branchId' });
 
-// LoanRepayment ↔ Loan
 db.LoanRepayment.belongsTo(db.Loan, { foreignKey: 'loanId' });
 db.Loan.hasMany(db.LoanRepayment, { foreignKey: 'loanId' });
 
-// LoanPayment ↔ Loan
 db.LoanPayment.belongsTo(db.Loan, { foreignKey: 'loanId' });
 db.Loan.hasMany(db.LoanPayment, { foreignKey: 'loanId' });
 
-// LoanPayment ↔ User (who recorded the payment)
 db.LoanPayment.belongsTo(db.User, { foreignKey: 'userId' });
 db.User.hasMany(db.LoanPayment, { foreignKey: 'userId' });
 
-// Export Sequelize and db object
+// Finalize
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
