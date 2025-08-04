ccbe82d4e22102baaaf75936c0f1179f2fc850d1
John Meena <johstamicrofinance@gmail.com>
2025-08-04 17:57:01 +0300

    ✅ Fix folder structure and clean loanRoutes reference

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -3,15 +3,12 @@ const router = express.Router();
 const loanController = require('../controllers/loanController');
 const { authenticateToken } = require('../middleware/authMiddleware');
 
-// Static routes (must go first)
+// Must be defined and exported!
 router.get('/disbursements/list', authenticateToken, loanController.getDisbursementList);
-router.get('/:loanId/schedule', authenticateToken, loanController.getLoanSchedule);
 
-// Main routes
+router.get('/:loanId/schedule', authenticateToken, loanController.getLoanSchedule);
 router.post('/', authenticateToken, loanController.createLoan);
 router.get('/', authenticateToken, loanController.getAllLoans);
-
-// Dynamic routes
 router.get('/:id', authenticateToken, loanController.getLoanById);
 router.put('/:id', authenticateToken, loanController.updateLoan);
 router.delete('/:id', authenticateToken, loanController.deleteLoan);
