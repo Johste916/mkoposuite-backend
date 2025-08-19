1cb41b69abe279f60b102b712cdd5224df93b85a
John Meena <johstamicrofinance@gmail.com>
2025-08-19 17:28:14 +0300

    All changes

---
+++ src/middleware/upload.js
@@ -0,0 +1,10 @@
+// src/middleware/upload.js
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + '-' + file.originalname.replace(/\s+/g, '_')),
+});
+module.exports = multer({ storage });
